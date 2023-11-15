478b7ed8d69d823cbab13a9e6256581e93869aa3
saroro <hbhj4633@naver.com>
2023-11-15 23:58:55 +0900

    2.4.1 업데이트

--- modules/DBManager/utils/decrypt/decrypt-new.d.ts
+++ modules/DBManager/utils/decrypt/decrypt-new.d.ts
@@ -1,3 +1,5 @@
+export declare const toCharArray: (chars: any) => string[];
+export declare const toByteArray: (bytes: any) => any;
 declare const Long: typeof java.lang.Long;
 export declare const decryptNew: (userId: number | string | typeof Long, enc: number, text: string) => string;
 export {};
--- modules/DBManager/utils/decrypt/decrypt.d.ts
+++ modules/DBManager/utils/decrypt/decrypt.d.ts
@@ -1,2 +1,1 @@
-export declare function decryptInit(packageName: string): void;
 export declare function decrypt(userId: number | string, enc: number, text: string): string;
